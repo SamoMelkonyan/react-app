18ba23af139f3acccdb3a19b25767a57a6b74db3
SamoMelkonyan <melqonyan98@gmail.com>
2019-11-22 19:53:57 +0400

    react changes

--- src/components/Companies/Index/index.js
+++ src/components/Companies/Index/index.js
@@ -1,6 +1,6 @@
-import React , {Component} from 'react';
-import {Link} from 'react-router-dom';
-import './index.scss';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./index.scss";
 import Api from "../../../api";
 import CompaniesItem from "./item";
 import Paginate from "../../BaseComponents/paginate";
@@ -13,13 +13,13 @@ export default class Companies extends Component {
             data: [],
             currentPage: null,
             total: null,
-            removed : false,
+            removed: false
         }
     };
     componentDidMount() {
         this.api.getCompanies().then(res => {
             this.setState({
-                companies : {
+                companies: {
                     data: res.data.data,
                     currentPage: res.data.current_page,
                     total: res.data.last_page,
@@ -28,67 +28,82 @@ export default class Companies extends Component {
             });
         });
     }
-    paginate(page){
+    paginate(page) {
         this.api.getCompanies(page).then(res => {
             this.setState({
-                companies : {
+                companies: {
                     data: res.data.data,
                     currentPage: res.data.current_page,
                     total: res.data.last_page,
-                    removed: false,
+                    removed: false
                 }
             });
         });
     }
-    deleteCompany(id){
-        if(!window.confirm('Are you sure?')){
+    deleteCompany(id) {
+        if (!window.confirm("Are you sure?")) {
             return false;
         }
-        this.api.destroyCompany(id).then(response => {
-            let data = [...this.state.companies.data];
-            data = data.filter(company => company.id !== id);
-            this.setState({
-                companies : {
-                    ...this.state.companies ,
-                    data ,
-                    removed : response.data.success,
-                }
+        this.api
+            .destroyCompany(id)
+            .then(response => {
+                let data = [...this.state.companies.data];
+                data = data.filter(company => company.id !== id);
+                this.setState({
+                    companies: {
+                        ...this.state.companies,
+                        data,
+                        removed: response.data.success
+                    }
+                });
             })
-        }).catch(err => {
-            console.error(err.response.data)
-        })
+            .catch(err => {
+                console.error(err.response.data);
+            });
     }
     render() {
-        const {data , total , currentPage , removed} = this.state.companies;
-        return(
+        const { data, total, currentPage, removed } = this.state.companies;
+        return (
             <div className="container companies-container mt-5">
-                <SuccessMessage message='The company has been successfully removed' success={removed} />
-                <Link to='/companies/create' className="btn btn-success mb-1" >Create Company</Link>
+                <SuccessMessage
+                    message="The company has been successfully removed"
+                    success={removed}
+                />
+                <Link to="/companies/create" className="btn btn-success mb-1">
+                    Create Company
+                </Link>
                 <div className="table-responsive">
                     <table className="table table-striped table-dark text-center text-nowrap">
                         <thead>
-                        <tr>
-                            <td>Id</td>
-                            <td>Name</td>
-                            <td>Email</td>
-                            <td>Logo</td>
-                            <td>Website</td>
-                            <td>Actions</td>
-                        </tr>
+                            <tr>
+                                <td>Id</td>
+                                <td>Name</td>
+                                <td>Email</td>
+                                <td>Logo</td>
+                                <td>Website</td>
+                                <td>Actions</td>
+                            </tr>
                         </thead>
                         <tbody>
-                            <CompaniesItem items={data} delete={(id) => this.deleteCompany(id)}/>
+                            <CompaniesItem
+                                items={data}
+                                delete={id => this.deleteCompany(id)}
+                            />
                         </tbody>
                     </table>
                 </div>
                 <div className="d-flex justify-content-center">
                     <nav aria-label="Page navigation example">
                         <ul className="pagination">
-                            <Paginate total={total} currentPage={currentPage} click={(page) => this.paginate(page)} />
+                            <Paginate
+                                total={total}
+                                currentPage={currentPage}
+                                click={page => this.paginate(page)}
+                            />
                         </ul>
                     </nav>
                 </div>
             </div>
-        )
+        );
     }
 }
--- src/components/Employees/Edit/index.js
+++ src/components/Employees/Edit/index.js
@@ -1,151 +1,176 @@
-import React , {Component} from 'react';
-import './index.scss';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import "./index.scss";
+import { Link } from "react-router-dom";
 import Api from "../../../api";
 import ErrorMessage from "../../BaseComponents/errorMessage";
 import SuccessMessage from "../../BaseComponents/successMessage";
-
 
 export default class EmployeesEdit extends Component {
     api = new Api();
 
     state = {
-        first_name : '',
-        last_name : '',
-        companies_id : '',
-        email : '',
-        phone : '',
-        companiesList : [],
-        errors : [],
-        success : false,
+        first_name: "",
+        last_name: "",
+        companies_id: "",
+        email: "",
+        phone: "",
+        companiesList: [],
+        errors: [],
+        success: false
     };
 
     componentDidMount() {
-        const {id} = this.props.match.params;
-        if(isNaN(id)){
-            this.props.history.push('/error-404')
+        const { id } = this.props.match.params;
+        if (isNaN(id)) {
+            this.props.history.push("/error-404");
         }
 
-        this.api.getEmployee(id).then(response => {
-            let data = {};
-            Object.keys(response.data).map((key) => {
-                if(response.data[key] != null) {
-                    return data[key] = response.data[key]
-                }else{
-                    return false
-                }
-            });
-            this.setState({
-                ...data
-            });
-            this.api.getCompaniesForEmployee().then(res => {
+        this.api
+            .getEmployee(id)
+            .then(response => {
+                let data = {};
+                Object.keys(response.data).map(key => {
+                    if (response.data[key] != null) {
+                        return (data[key] = response.data[key]);
+                    } else {
+                        return false;
+                    }
+                });
                 this.setState({
-                    companiesList : res.data
-                })
+                    ...data
+                });
+                this.api.getCompaniesForEmployee().then(res => {
+                    this.setState({
+                        companiesList: res.data
+                    });
+                });
+            })
+            .catch(err => {
+                if (err.response.status === 404) {
+                    this.props.history.push("/error-404");
+                }
+                console.error(err.response.data);
             });
-        }).catch(err => {
-            if(err.response.status === 404){
-                this.props.history.push('/error-404')
-            }
-            console.error(err.response.data)
-        });
-
     }
 
-    handleSubmit = (e) => {
+    handleSubmit = e => {
         e.preventDefault();
-        const {id} = this.props.match.params;
+        const { id } = this.props.match.params;
         const formData = new FormData();
-        formData.append('first_name' , this.state.first_name);
-        formData.append('last_name' , this.state.last_name);
-        formData.append('companies_id' , this.state.companies_id);
-        formData.append('email', this.state.email);
-        formData.append('phone' , this.state.phone);
-        formData.append('_method' , 'PUT');
+        formData.append("first_name", this.state.first_name);
+        formData.append("last_name", this.state.last_name);
+        formData.append("companies_id", this.state.companies_id);
+        formData.append("email", this.state.email);
+        formData.append("phone", this.state.phone);
+        formData.append("_method", "PUT");
 
-        this.api.updateEmployee(id , formData)
+        this.api
+            .updateEmployee(id, formData)
             .then(response => {
                 this.setState({
-                    errors : [],
-                    success : response.data.success,
+                    errors: [],
+                    success: response.data.success
                 });
             })
             .catch(err => {
                 return this.setState({
-                    errors : Object.values(err.response.data.errors),
-                    success : false,
+                    errors: Object.values(err.response.data.errors),
+                    success: false
                 });
             });
     };
-    handleChange = (e) => {
+    handleChange = e => {
         const value = e.target.value;
         const name = e.target.name;
         this.setState({
-            [name]: value,
+            [name]: value
         });
     };
 
     render() {
-        return(
+        return (
             <div className="container mt-5 employee-edit-container">
-                <Link to="/employees" className="back-link"><i className="fa fa-chevron-circle-left" /></Link>
-                <SuccessMessage success={this.state.success} message='The employee has been successfully edited' />
-                <ErrorMessage errors={this.state.errors}/>
+                <Link to="/employees" className="back-link">
+                    <i className="fa fa-chevron-circle-left" />
+                </Link>
+                <SuccessMessage
+                    success={this.state.success}
+                    message="The employee has been successfully edited"
+                />
+                <ErrorMessage errors={this.state.errors} />
 
                 <h1 className="text-center mb-3">Edit Employee</h1>
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="first_name">Name *</label>
-                        <input type="text"
-                               className="form-control"
-                               name="first_name"
-                               id="first_name"
-                               placeholder="First Name"
-                               value={this.state.first_name}
-                               onChange={this.handleChange} />
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="first_name"
+                            id="first_name"
+                            placeholder="First Name"
+                            value={this.state.first_name}
+                            onChange={this.handleChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="last_name">Last Name *</label>
-                        <input type="text"
-                               className="form-control"
-                               name="last_name"
-                               id="last_name"
-                               placeholder="Last Name"
-                               value={this.state.last_name}
-                               onChange={this.handleChange} />
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="last_name"
+                            id="last_name"
+                            placeholder="Last Name"
+                            value={this.state.last_name}
+                            onChange={this.handleChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="company">Company</label>
-                        <select name="companies_id" value={this.state.companies_id} id="companies_id" onChange={this.handleChange}  className='custom-select'>
+                        <select
+                            name="companies_id"
+                            value={this.state.companies_id}
+                            id="companies_id"
+                            onChange={this.handleChange}
+                            className="custom-select"
+                        >
                             <option value="">Select Company</option>
-                            {this.state.companiesList.map(company =>
-                                <option key={company.id} value={company.id}>{company.name}</option>
-                            )}
+                            {this.state.companiesList.map(company => (
+                                <option key={company.id} value={company.id}>
+                                    {company.name}
+                                </option>
+                            ))}
                         </select>
                     </div>
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="text"
-                               className="form-control"
-                               name="email"
-                               id="email"
-                               placeholder="Email"
-                               value={this.state.email}
-                               onChange={this.handleChange} />
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="email"
+                            id="email"
+                            placeholder="Email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="phone">Phone</label>
-                        <input type="text"
-                               className="form-control"
-                               name="phone"
-                               id="phone"
-                               placeholder="Phone"
-                               value={this.state.phone}
-                               onChange={this.handleChange} />
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="phone"
+                            id="phone"
+                            placeholder="Phone"
+                            value={this.state.phone}
+                            onChange={this.handleChange}
+                        />
                     </div>
-                    <button type="submit" className="btn btn-success w-100">Edit</button>
+                    <button type="submit" className="btn btn-success w-100">
+                        Edit
+                    </button>
                 </form>
             </div>
-        )
+        );
     }
 }
